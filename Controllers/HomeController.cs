f62b5b4456a9e3d203b3de7b74bbb504351886f9
Nasif Shahrier <nasifshahrier.cse@gmail.com>
2026-07-02 15:37:46 +0600

    Initial Commit: MVC setup and Patiend model added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Hospital_Management_System.Models;
+
+namespace Hospital_Management_System.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
